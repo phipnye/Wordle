f294fa8ea73836191dd442d392cd713075254f3f
phipnye <75276332+phipnye@users.noreply.github.com>
2022-03-10 16:21:03 -0800

    Add files via upload

--- wordle_solver.py
+++ wordle_solver.py
@@ -4,12 +4,7 @@ import sys
 import time
 
 #Import Possible Words
-global word_list
 word_list = pd.read_csv("word_list.txt", delimiter = "\n", header=None)
-
-#Declare list of letters that are not contained anywhere
-global not_contained
-not_contained = []
 
 #Declare list of letters that are invalid for a given position
 impos1 = []
@@ -19,17 +14,13 @@ impos4 = []
 impos5 = []
 
 #List of the letters that must be contained using the unique values in the impossibilities lists
-global must_contain
 must_contain = []
 
 #Dictionary of the correct word
-global correct_word
 correct_word = [None] * 5
 
 #Initialize an array of the impossibilities for each position
-global impossibilities
-impossibilities = np.empty(5, dtype=object)
-impossibilities[:] = impos1, impos2, impos3, impos4, impos5
+impossibilities = [impos1, impos2, impos3, impos4, impos5]
 
 """
 Algorithm that parses through the word list and assigns each word a score based on the number of entries that share the
@@ -83,9 +74,10 @@ feedback()
 
 for i in range(5):
     if Input[i] == '_':
-        not_contained = not_contained + [first_guess[i]]
+        for i in range(5):
+            impossibilities[i] += [first_guess[i]]
     elif Input[i] == '/':
-        impossibilities[i,] += [first_guess[i]]
+        impossibilities[i] += [first_guess[i]]
         if first_guess[i] not in must_contain:
            must_contain += [first_guess[i]]
     elif Input[i] == '*':
@@ -94,13 +86,9 @@ for i in range(5):
     else:
         sys.exit("Invalid feedback entry.")
 
-#Add wrong letters to the list of impossibilities
-for ele in impossibilities:
-    ele += not_contained
-
 #Filter out results that contain impossible letter entries
 for i in range(5):
-    word_list = word_list[~np.isin(word_list[0].str[i], impossibilities[i])]
+    word_list = word_list[word_list[0].str[i] not in impossibilities[i]]
 
 #If we know certain letters must be in the word, remove words that don't contain such letters
 if(len(must_contain) != 0):
@@ -119,40 +107,38 @@ while True:
             word_list = word_list[word_list[0].str[i] == new_guess[i]]
             correct_word[i] = new_guess[i]
         elif Input[i] == '/':
-            impossibilities[i,] += [new_guess[i]]
+            impossibilities[i] += [new_guess[i]]
             if new_guess[i] not in must_contain:
                 must_contain += [new_guess[i]]
             for ind in [i-4,i-3,i-2,i-1]:
                 if new_guess[ind] == new_guess[i] and (Input[ind] == '/' or Input[ind] == "*"):
                     word_list = word_list[word_list[0].str.count(new_guess[i]) > 1]
         elif Input[i] == '_':
-            if new_guess[i] not in must_contain and new_guess[i] not in correct_word and new_guess[i] not in not_contained:
+            if new_guess[i] not in must_contain and new_guess[i] not in correct_word and all(new_guess[i] not in impossibilities[x] for x in range(5)):
                 if not any(new_guess[i] == new_guess[i - x] for x in range(1,5)):
-                        not_contained += [new_guess[i]]
+                    for i in range(5):
+                        impossibilities[i] += [new_guess[i]]
                 else:
                     for ind in [i-1,i-2,i-3,i-4]:
                         if new_guess[ind] == new_guess[i] and (Input[ind] == '_'):
-                            not_contained += [new_guess[i]]
+                            impossibilities[i] += [new_guess[i]]
                         else:
                             word_list = word_list[word_list[0].str.count(new_guess[i]) == sum([(new_guess[i-x] == new_guess[i]) and (Input[i-x] != '_') for x in range(1,5)])]
-            elif new_guess[i] in not_contained:
+                            break
+            elif all(new_guess[i] in impossibilities[x] for x in range(5)):
                 break
             elif new_guess[i] in must_contain:
-                impossibilities[i,] += [new_guess[i]]
+                impossibilities[i] += [new_guess[i]]
                 word_list = word_list[word_list[0].str.count(new_guess[i]) == 1]
             else:
                 for ind in [correct_word.index(new_guess[i])-4,correct_word.index(new_guess[i])-3,correct_word.index(new_guess[i])-2,correct_word.index(new_guess[i])-1]:
-                    impossibilities[ind,] += [new_guess[i]]   
+                    impossibilities[ind] += [new_guess[i]]   
         else:
             sys.exit("Invalid feedback entry.")
 
-    #Add wrong letters to the list of impossibilities
-    for ele in impossibilities:
-        ele += not_contained
-
     #Filter out results that contain impossible letter entries
     for i in range(5):
-        word_list = word_list[~np.isin(word_list[0].str[i], impossibilities[i])]
+        word_list = word_list[word_list[0].str[i] not in impossibilities[i]]
 
     #If we know certain letters must be in the word, remove words that don't contain such letters
     if(len(must_contain) != 0):
